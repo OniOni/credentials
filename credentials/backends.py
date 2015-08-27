803c9bdadef0229a4fe405fc1d1bce6256b4f1e0
OniOni <mathieu.c.sabourin@gmail.com>
2015-08-26 18:52:55 -0700

    Add json file backend.

--- credentials/backends.py
+++ credentials/backends.py
@@ -1,7 +1,20 @@
 import os
+import json
 
 
 class EnvBackend(object):
 
     def load(self, key):
         return os.getenv(key)
+
+
+class JsonFileBackend(object):
+
+    def __init__(self, path):
+        self._path = path
+
+    def load(self, key):
+        with open(self._path, 'r') as f:
+            doc = json.load(f)
+
+        return doc.get(key, None)
