12af9ff07bc4f351429c1d9c0dbe7570cdf76b69
OniOni <mathieu.c.sabourin@gmail.com>
2015-09-24 15:28:08 -0700

    Add more test for JsonFileBackend.

--- tests/test.py
+++ tests/test.py
@@ -106,16 +106,23 @@ class TestEnvBackend(unittest.TestCase):
         self.assertEqual(cred, None)
 
 
-class TestPatchedJsonFileBackend(BasePatchTest, unittest.TestCase):
+class BasePatchedJsonFileBackend(BasePatchTest):
 
     def setUp(self):
         self.open = mock.mock_open(
-            read_data='{"key":42}'
+            read_data=self.read_data
         )
         self.patch = mock.patch('credentials.backends.open',
                                 self.open, create=True)
         self.backend = credentials.JsonFileBackend('ignore')
-        super(TestPatchedJsonFileBackend, self).setUp()
+        super(BasePatchedJsonFileBackend, self).setUp()
+
+
+class GoodPatchedJFBackend(BasePatchedJsonFileBackend, unittest.TestCase):
+
+    def setUp(self):
+        self.read_data = '{"key":42}'
+        super(GoodPatchedJFBackend, self).setUp()
 
     def test_simple(self):
         cred = self.backend.load('key')
@@ -124,6 +131,31 @@ class TestPatchedJsonFileBackend(BasePatchTest, unittest.TestCase):
     def test_e2e(self):
         cred = self.loader.load('key')
         self.assertEqual(cred, 42)
+
+    def test_missing_key_returns_none(self):
+        cred = self.backend.load('not_a_key')
+        self.assertEqual(cred, None)
+
+
+class BadPatchedJFBackend(BasePatchedJsonFileBackend, unittest.TestCase):
+
+    def setUp(self):
+        self.read_data = 'Not valid json'
+        super(BadPatchedJFBackend, self).setUp()
+
+    def test_malformed_json_file(self):
+        cred = self.backend.load('key')
+        self.assertEqual(cred, None)
+
+
+class TestJsonFileBackend(unittest.TestCase):
+
+    def setUp(self):
+        self.backend = credentials.JsonFileBackend('absent')
+
+    def test_absent_json_file(self):
+        cred = self.backend.load('key')
+        self.assertEqual(cred, None)
 
 if __name__ == "__main__":
     unittest.main()
