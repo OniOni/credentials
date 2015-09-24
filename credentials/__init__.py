278ebe938392e4bf18d79873b3f84feee6c7cf59
OniOni <mathieu.c.sabourin@gmail.com>
2015-09-24 12:28:24 -0700

    Avoid namespace collision.

--- credentials/__init__.py
+++ credentials/__init__.py
@@ -3,5 +3,8 @@ import os
 from .base import Credentials
 from .backends import EnvBackend, JsonFileBackend
 
-backends = [EnvBackend(), JsonFileBackend(os.path.expanduser('~/.credentials.json'))]
-credentials = Credentials(backends)
+active_backends = [
+    EnvBackend(),
+    JsonFileBackend(os.path.expanduser('~/.credentials.json'))
+]
+credentials = Credentials(active_backends)
