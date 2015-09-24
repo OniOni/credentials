11a1e5217a62a36c3c989e02dcde2cd84294e2e2
OniOni <mathieu.c.sabourin@gmail.com>
2015-09-24 15:50:33 -0700

    Better pypi description and such.

--- setup.py
+++ setup.py
@@ -1,9 +1,16 @@
 from setuptools import setup
 
+with open('README.md', 'r') as f:
+    LONG_DESCRIPTION = f.read()
+
 setup(
     name='credentials',
+    description='Credential Manager',
+    long_description=LONG_DESCRIPTION,
     author='Mathieu Sabourin',
+    author_email='mathieu.c.sabourin@gmail.com',
+    url='https://github.com/OniOni/credentials',
     packages=['credentials'],
-    version=1.0,
+    version=1.1,
     license='LGPLv3'
 )
